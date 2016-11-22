97ceb76366440df1a7fda62bb885a7c78371b3f2
Raghu Betina <raghu@firstdraft.com>
2016-11-22 20:00:10 +0000

    Whitelist columns in Bean for ActiveAdmin

--- app/admin/bean.rb
+++ app/admin/bean.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Bean do
 
+ permit_params :name, :roaster_id, :website_url, :image_url, :flavor_note_1, :flavor_note_2, :flavor_note_3, :price_per_bag, :std_bag_size_in_ounces, :elevation_low, :elevation_high, :process, :varietal, :origin_country_id, :caffine, :blend, :espresso
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
