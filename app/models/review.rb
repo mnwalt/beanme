c83278c033871e459f73a754e0c25e894ea1942a
Raghu Betina <raghu@firstdraft.com>
2016-11-22 19:59:24 +0000

    Validate numericality of reviews.overall_rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,8 @@ class Review < ApplicationRecord
 
   # Validations
 
+  validates :overall_rating, :numericality => { :less_than_or_equal_to => 5, :greater_than_or_equal_to => 0 }
+
   validates :user_id, :presence => true
 
 end
