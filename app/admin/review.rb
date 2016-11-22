4a5a6c3077b8cbc0d2201ce6c9e3c5b86f809c23
Raghu Betina <raghu@firstdraft.com>
2016-11-22 20:00:35 +0000

    Whitelist columns in Review for ActiveAdmin

--- app/admin/review.rb
+++ app/admin/review.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Review do
 
+ permit_params :user_id, :bean_id, :overall_rating, :comments, :image
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
